4e843b5672dd8d143fccd804119438f655f2871f
wnpraf <rapribu@gmail.com>
2019-02-01 17:55:13 +0100

    First commit. Creating random stock data

---
+++ src/data/products.js
@@ -0,0 +1,29 @@
+let idTracker = 1
+
+const fakeId = () => idTracker++
+const randomInterval = (min, max) => min + Math.random() * (max - min)
+const randomPrize = (min, max) => {
+  const prize = randomInterval(min, max)
+  return prize.toFixed(2)
+}
+const randomStock = (min, max) => {
+  const stock = randomInterval(min, max)
+  return Math.round(stock)
+}
+const abc = 'abcdefghijkelmnñopqrstuvwyz'
+
+const makeProducts = (str) => {
+  const products = []
+  for (let name of abc) {
+    const product = {
+      id: fakeId(),
+      name: `product-${name.toUpperCase()}`,
+      prize: randomPrize(60, 150)
+    }
+    products.push(product)
+  }
+  return products
+}
+const products = makeProducts(abc)
+
+const stock = products.map(e => ({ ProductId: e.id, stock: randomStock(8, 15) }))
