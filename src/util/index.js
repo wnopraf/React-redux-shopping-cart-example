f81f92f9d6834d33c271b1cd10751782e26161b6
wnpraf <rapribu@gmail.com>
2019-02-15 19:59:29 +0100

    Adding pagination utils and its tests

--- src/util/index.js
+++ src/util/index.js
@@ -1,1 +1,14 @@
 export const selectIdItem = (stateSlice, id) => stateSlice.find(e => e.productId === id)
+
+export function pageButtons (data, itemsPerPage = 6) {
+  const totalProducts = data.length
+  const totalPages = totalProducts % itemsPerPage === 0 ? totalProducts / itemsPerPage : Math.floor(totalProducts / itemsPerPage + 1)
+  return totalPages
+}
+
+export function paginate (data, page, itemsPerPage = 6) {
+  const sliceEnd = itemsPerPage * page
+  const sliceStart = sliceEnd - itemsPerPage
+  const products = data.slice(sliceStart, sliceEnd)
+  return products
+}
---
+++ src/util/util.test.js
@@ -0,0 +1,54 @@
+import { paginate, pageButtons } from './index'
+
+describe('Testing pagination utils', () => {
+  describe('items per page function', () => {
+    const items = new Array(16).fill(0).map((e, i) => i + 1)
+    describe('1-page', () => {
+      let slice
+      test('Has the right lenght', () => {
+        slice = paginate(items, 1)
+        expect(slice).toHaveLength(6)
+      })
+      test('Has the right items', () => {
+        slice.forEach((e, i) => {
+          expect(e).toBe(i + 1)
+        })
+      })
+    })
+    describe('2-pages', () => {
+      let slice
+      test('Has the right lenght', () => {
+        slice = paginate(items, 2)
+        expect(slice).toHaveLength(6)
+      })
+      test('Has the right items', () => {
+        slice.forEach((e, i) => {
+          expect(e).toBe(i + 7)
+        })
+      })
+    })
+    describe('Last page', () => {
+      let slice
+      test('Has the right lenght', () => {
+        slice = paginate(items, 3)
+        expect(slice).toHaveLength(4)
+      })
+      test('Has the right items', () => {
+        slice.forEach((e, i) => {
+          expect(e).toBe(i + 13)
+        })
+      })
+    })
+    describe.only('Page buttons', () => {
+      test('Has the right number of pages', () => {
+        const pages = pageButtons(items)
+        expect(pages).toBe(3)
+      })
+      test('Has even number of pages', () => {
+        const items = new Array(24).fill(0).map((e, i) => i + 1)
+        const pages = pageButtons(items)
+        expect(pages).toBe(4)
+      })
+    })
+  })
+})
